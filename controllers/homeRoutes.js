14d2236582020229a752e9924b657bf4276259c8
TDiaz83 <tomas.enrique.diaz@gmail.com>
2022-11-11 11:17:13 -0600

    homeRoute, homepage working - needs CSS formatting

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,12 +1,42 @@
 const router = require('express').Router();
+const { User, Listing, SavedListing, Review } = require('../models');
 
 router.get('/', async (req, res) => {
-    res.render('home');
+    try {
+        const listingData = await Listing.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name', 'id']
+                },
+                {
+                    model: Review,
+                    attributes: ['comment'],
+                    include: [
+                        {
+                            model: User, 
+                            attributes: ['name', 'id']
+                        }
+                    ]
+                },
+            ]
+        })
+
+        const listings = listingData.map((listing) => listing.get({ plain: true }));
+
+        //res.status(200).json(listingData);
+        res.render('home', {
+            listings
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 });
 
-// router.get('/', async (req, res) => {
-//     res.render('contact');
-// });
+router.get('/contact', async (req, res) => {
+    res.render('contact');
+});
 
 // router.get('/', async (req, res) => {
 //     res.render('dashboard');

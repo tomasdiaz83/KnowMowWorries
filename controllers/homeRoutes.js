12f4724336783665c3fa2f3072b22aef94e932cf
TDiaz83 <tomas.enrique.diaz@gmail.com>
2022-11-11 10:38:05 -0600

    homeroutes built, but not working

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+
+router.get('/', async (req, res) => {
+    res.render('contact');
+});
+
+// router.get('/', async (req, res) => {
+//     res.render('contact');
+// });
+
+// router.get('/', async (req, res) => {
+//     res.render('dashboard');
+// });
+
+// router.get('/', async (req, res) => {
+//     res.render('postlogin');
+// });
+
+// router.get('/', async (req, res) => {
+//     res.render('search');
+// });   
+   
+
+module.exports = router;

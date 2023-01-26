0a0d60faab2eccc83487a2ffc096a666eb74ce21
TDiaz83 <tomas.enrique.diaz@gmail.com>
2023-01-26 13:47:19 -0600

    Attempting some sequelize magic with Render - making db, user, and pass explicit

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ let sequelize
 
 if (process.env.RENDER) {
   console.log("Got to Render")
-  sequelize = new Sequelize(process.env.RENDERDB, null, null, {});
+  sequelize = new Sequelize('kmw_db', 'kmw_db_user', 'L1mNsFEhJQh0m3XQ1n3qHUc7XwQCbXeA');
 } else if (process.env.CYCLIC_URL) {
   console.log("Got to CYCLIC")
 } else {
